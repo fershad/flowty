8ff139a594297508fd863fdaf58b03b73de9e1c2
Ronny Shani <63248335+ironnysh@users.noreply.github.com>
2022-09-30 12:00:50 +0000

    Add Vercel URL

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "webflowUrl": "https://ronnys-supercool-site.webflow.io", // Example: https://my-website.webflow.io
-    "customDomain": "", // Example: https://my-website.com
+    "customDomain": "https://flowty-demo-five.vercel.app", // Example: https://my-website.com
     "css": {
         "critical": false,
         "purge": false
